b26e300cb3a5c96628710683a7729f0ad19c9ca0
victoria-lokteva <74068173+victoria-lokteva@users.noreply.github.com>
2022-11-19 21:17:25 +0300

    Update avg_salaries.py

--- SQL/avg_salaries.py
+++ SQL/avg_salaries.py
@@ -1,1 +1,9 @@
 
+def avg_salaries(salaries: list, json_name: str = "salaries.json"):
+    salaries = [x.split(' ') for x in salaries]
+    salaries = [(x[0], int(x[1]), x[2]) for x in salaries]
+    columns = ["Name", "Salary", "Month"]
+    dataframe = spark.createDataFrame(salaries, columns)
+    mean_salaries = dataframe.groupBy("Name").avg("Salary")
+    mean_salaries.write.json(json_name)
+    return mean_salaries
