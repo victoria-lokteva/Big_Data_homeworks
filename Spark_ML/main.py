a26b77dd0ab0814e0942ac5b18259672c8052d1c
victoria-lokteva <74068173+victoria-lokteva@users.noreply.github.com>
2022-12-05 23:29:24 -0300

    Update main.py

--- Spark_ML/main.py
+++ Spark_ML/main.py
@@ -1,6 +1,8 @@
 from pyspark.sql import functions as F
 from pyspark.sql.types import StructType, StructField, IntegerType, StringType
-from pyspark.ml.classification import LogisticRegression
+from pyspark.ml.classification import LogisticRegression, LinearSVC, NaiveBayes, RandomForestClassifier
+from pyspark.ml.tuning import CrossValidator, ParamGridBuilder, CrossValidatorModel
+from pyspark.ml.feature import StringIndexer, OneHotEncoder, VectorAssembler
 
 name = '/Users/victorialokteva/Downloads/titanic.csv'
 schema = StructType([
@@ -23,4 +25,20 @@ df = spark.read.csv(name, sep =';', header=True, schema=schema)
 train, test = df.randomSplit([0.6, 0.4], seed=2)
 
 
-lr = LogisticRegression()
+lr = LinearRegression()
+lr_model = lr.fit(train)
+
+
+lsvc = LinearSVC(maxIter=10, regParam=0.1)
+lsvc = lsvc.fit(train)
+
+
+nb = NaiveBayes(smoothing=1.0, modelType="multinomial")
+nb = nb.fit(train)
+
+
+rf = RandomForestClassifier(seed=42)
+grid = ParamGridBuilder().addGrid(rf.maxDepth, [1, 4]).addGrid(rf.numTrees, [3, 15]).build()
+evaluator = BinaryClassificationEvaluator()
+cv = CrossValidator(estimator=rf, estimatorParamMaps=grid, evaluator=evaluator,
+    parallelism=2)
