c362a3d0e8f1ec4da304b973a1c240358a3f7efa
victoria-lokteva <74068173+victoria-lokteva@users.noreply.github.com>
2022-12-05 22:32:53 -0300

    Update main.py

--- Spark_ML/main.py
+++ Spark_ML/main.py
@@ -1,1 +1,20 @@
+from pyspark.sql import functions as F
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType
+name = '/Users/victorialokteva/Downloads/titanic.csv'
+schema = StructType([
+    StructField("pclass", IntegerType(), True),
+    StructField("survived", IntegerType(), True),
+    StructField("name", StringType(), True),
+    StructField("sex", StringType(), True),
+    StructField("age", IntegerType(), True),
+    StructField("sibsp", IntegerType(), True),
+    StructField("parch", IntegerType(), True),
+    StructField("ticket", IntegerType(), True),
+    StructField("fare", IntegerType(), True),
+    StructField("cabin", StringType(), True),
+    StructField("embarked", StringType(), True),
+    StructField("boat", IntegerType(), True),
+    StructField("body", IntegerType(), True),
+    StructField("homedest", StringType(), True)])
 
+df = spark.read.csv(name, sep =';', header=False,schema=schema)
