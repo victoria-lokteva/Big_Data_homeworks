dbcd42fdef8a00a0e2d57dc5ec924b349a3bc70c
victoria-lokteva <74068173+victoria-lokteva@users.noreply.github.com>
2022-12-11 22:08:39 -0300

    Update main.py

--- Spark_ML/main.py
+++ Spark_ML/main.py
@@ -30,7 +30,7 @@ features = ["cap-shape", "cap-surface", "cap-color", "bruises", "odor",
        "gill-attachment", "gill-spacing", "gill-size", "gill-color",
        "stalk-shape", "stalk-root", "stalk-surface-above-ring",
        "stalk-surface-below-ring", "stalk-color-above-ring",
-       "stalk-color-below-ring", "veil-type", "veil-color", "ring-number",
+       "stalk-color-below-ring", "veil-color", "ring-number",
        "ring-type", "spore-print-color", "population", "habitat"]
 
 # превратим строки в числа
